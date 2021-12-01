3137e688b8332bf3ed3b3076ecff80f8bebb6766
Halit <egitimtraining@gmail.com>
2021-12-01 14:39:24 +0300

    Veteriner Hekime Soru Sor Ekranın Tasarlanması

--- app/src/main/java/com/halit/veterinaryapphalit/Fragments/HomeFragment.java
+++ app/src/main/java/com/halit/veterinaryapphalit/Fragments/HomeFragment.java
@@ -1,20 +1,23 @@
 package com.halit.veterinaryapphalit.Fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
 import com.halit.veterinaryapphalit.R;
 import com.halit.veterinaryapphalit.Utils.ChangeFragments;
 
 public class HomeFragment extends Fragment {
 
     private View view;
-    private LinearLayout petlerimLayout;
+    private LinearLayout petlerimLayout,soruSorLinerLayout;
     private ChangeFragments changeFragments;
 
     @Override
@@ -29,6 +32,7 @@ public class HomeFragment extends Fragment {
 
     public void tanimla(){
         petlerimLayout = view.findViewById(R.id.petlerimLayout);
+        soruSorLinerLayout = view.findViewById(R.id.soruSorLinerLayout);
         changeFragments = new ChangeFragments(getContext());
     }
 
@@ -39,6 +43,33 @@ public class HomeFragment extends Fragment {
                 changeFragments.change(new UserPetsFragment());
             }
         });
+        soruSorLinerLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuestionAlert();
+            }
+        });
+    }
+
+    public void openQuestionAlert(){
+        LayoutInflater layoutInflater = this.getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.soru_sor_alert_dialog_layout,null);
+
+        EditText soruSorEditText = view.findViewById(R.id.soruSorEditText);
+        MaterialButton soruSorButton = view.findViewById(R.id.soruSorButton);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        alert.setView(view);
+        alert.setCancelable(true);
+        final AlertDialog alertDialog = alert.create();
+        soruSorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.cancel();
+            }
+        });
+        alertDialog.show();
+
     }
 
 
