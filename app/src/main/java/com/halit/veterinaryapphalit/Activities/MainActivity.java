0c3f8d3996be2e5b925875cf2bbda6bfae3d61c1
Halit <egitimtraining@gmail.com>
2021-12-08 20:06:57 +0300

    Ana Sayfa ve Arama Yap Butonu Eklenmesi

--- app/src/main/java/com/halit/veterinaryapphalit/Activities/MainActivity.java
+++ app/src/main/java/com/halit/veterinaryapphalit/Activities/MainActivity.java
@@ -3,9 +3,11 @@ package com.halit.veterinaryapphalit.Activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
 import com.halit.veterinaryapphalit.Fragments.HomeFragment;
 import com.halit.veterinaryapphalit.R;
 import com.halit.veterinaryapphalit.Utils.ChangeFragments;
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences getSharedPreferences;
     private GetSharedPreferences preferences;
+    private MaterialButton anasayfaButton;
+    private ChangeFragments changeFragments;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +27,27 @@ public class MainActivity extends AppCompatActivity {
         getFragment();
         tanimla();
         kontrol();
+        action();
+    }
+
+    private void action() {
+        anasayfaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeFragments.change(new HomeFragment());
+            }
+        });
     }
 
     private void getFragment() {
-        ChangeFragments changeFragments = new ChangeFragments(MainActivity.this);
+        changeFragments = new ChangeFragments(MainActivity.this);
         changeFragments.change(new HomeFragment());
     }
 
     private void tanimla() {
         preferences = new GetSharedPreferences(MainActivity.this);
         getSharedPreferences = preferences.getSession();
+        anasayfaButton = findViewById(R.id.anasayfaButton);
     }
 
     public void kontrol(){
