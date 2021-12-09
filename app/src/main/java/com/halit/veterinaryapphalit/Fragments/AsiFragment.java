1297f67355f63d8d36277f39cdcd68efbb60675c
Halit <egitimtraining@gmail.com>
2021-12-09 23:24:19 +0300

    İşaretlenen Tarihlere Tklanınca Aşı Yapılacak Pet Hakkında Bilgi Ekranı Verilmesi

--- app/src/main/java/com/halit/veterinaryapphalit/Fragments/AsiFragment.java
+++ app/src/main/java/com/halit/veterinaryapphalit/Fragments/AsiFragment.java
@@ -1,16 +1,20 @@
 package com.halit.veterinaryapphalit.Fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.halit.veterinaryapphalit.Models.AsiModel;
 import com.halit.veterinaryapphalit.R;
 import com.halit.veterinaryapphalit.RestApi.ManagerAll;
+import com.halit.veterinaryapphalit.Utils.ChangeFragments;
+import com.squareup.picasso.Picasso;
 import com.squareup.timessquare.CalendarPickerView;
 
 import java.text.DateFormat;
@@ -21,6 +25,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -42,6 +47,7 @@ public class AsiFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_asi, container, false);
         tanimla();
         getAsi();
+        clickToCalendar();
         return view;
     }
 
@@ -63,21 +69,27 @@ public class AsiFragment extends Fragment {
         req.enqueue(new Callback<List<AsiModel>>() {
             @Override
             public void onResponse(Call<List<AsiModel>> call, Response<List<AsiModel>> response) {
-                if (response.isSuccessful()){
-                    asiList = response.body();
-                    for (int i = 0; i < asiList.size(); i++) {
-                        Log.i( "asilar",response.body().toString());
-                        String dataString = response.body().get(i).getAsitarih().toString();
-                        try {
-                            Date date = format.parse(dataString);
-                            dateList.add(date);
-                        } catch (ParseException e) {
-                            e.printStackTrace();
+                if (response.isSuccessful()) {
+                    if (response.body().get(0).isTf()) {
+                        asiList = response.body();
+                        for (int i = 0; i < asiList.size(); i++) {
+//                            Log.i("asilar", response.body().toString());
+                            String dataString = response.body().get(i).getAsitarih().toString();
+                            try {
+                                Date date = format.parse(dataString);
+                                dateList.add(date);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
                         }
+
+                        calendarPickerView.init(today, nextYear.getTime()).withHighlightedDates(dateList);
+
                     }
-
-                    calendarPickerView.init(today,nextYear.getTime()).withHighlightedDates(dateList);
-
+                }else {
+                    ChangeFragments changeFragments = new ChangeFragments(getContext());
+                    changeFragments.change(new HomeFragment());
+                    Toast.makeText(getContext(), "Petinize Ait Gelecek Tarihte Asi Yoktur...", Toast.LENGTH_LONG).show();
                 }
 
             }
@@ -87,5 +99,48 @@ public class AsiFragment extends Fragment {
 
             }
         });
+    }
+      public void clickToCalendar(){
+        calendarPickerView.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Date date) {
+                for (int i = 0; i <dateList.size(); i++ ){
+
+                    if (date.toString().equals(dateList.get(i).toString())){
+//                        Toast.makeText(getContext(), asiList.get(i).getPetisim().toString(), Toast.LENGTH_LONG).show();
+                        openQuestionAlert(asiList.get(i).getPetisim(),asiList.get(i).getPettur(),asiList.get(i).getAsitarih(), asiList.get(i).getAsiisim(),asiList.get(i).getPetresim());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onDateUnselected(Date date) {
+
+            }
+        });
+      }
+
+    public void openQuestionAlert(String petIsmi,String petTur,String tarihim,String asiIsmi, String resimUrl){
+        LayoutInflater layoutInflater = this.getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.asi_takip_layout,null);
+
+        TextView petIsimText = view.findViewById(R.id.petIsimText);
+        TextView petAsiTakipBilgiText = view.findViewById(R.id.petAsiTakipBilgiText);
+        CircleImageView asiTakipCircleImageImageView = view.findViewById(R.id.asiTakipCircleImageView);
+
+        petIsimText.setText(petIsmi);
+//        petAsiTakipBilgiText.setText(petIsmi + " isimli petinizin "+ tarihim + " tarihinde " + asiIsmi + " asisi yapilacaktir");
+        petAsiTakipBilgiText.setText(petIsmi + " isimli " + petTur + " " + tarihim + " tarihinde " + asiIsmi + " asisi yapilacaktir");
+
+        Picasso.get().load(resimUrl).into(asiTakipCircleImageImageView);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        alert.setView(view);
+        alert.setCancelable(true);
+        final AlertDialog alertDialog = alert.create();
+
+        alertDialog.show();
+
     }
 }
