8fa868cca750372aa0dffdeeae6f2443001bc0f5
Halit <egitimtraining@gmail.com>
2021-12-24 01:02:56 +0300

    Term nd Condition ve Layout Styles

--- app/src/main/java/com/halit/veterinaryapphalit/Activities/GirisYapActivity.java
+++ app/src/main/java/com/halit/veterinaryapphalit/Activities/GirisYapActivity.java
@@ -1,14 +1,24 @@
 package com.halit.veterinaryapphalit.Activities;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import com.halit.veterinaryapphalit.Models.LoginModel;
 import com.halit.veterinaryapphalit.R;
@@ -26,6 +36,10 @@ public class GirisYapActivity extends AppCompatActivity {
     private TextView loginText;
     private Button loginButton;
 
+    public static final String TERMS_AND_CONDITIONS = "TERMS_AND_CONDITIONS";
+    public static final String PLANT_PLACES_PREFS = "PLANT_PLACES_PREFS";
+//    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +47,44 @@ public class GirisYapActivity extends AppCompatActivity {
 
         tanimla();
         click();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(PLANT_PLACES_PREFS, Context.MODE_PRIVATE);
+        if (!sharedPreferences.getBoolean(TERMS_AND_CONDITIONS, false)) {
+            TermsAndConditionsDialogFragment tsandcs = new TermsAndConditionsDialogFragment();
+            Dialog dialog = tsandcs.onCreateDialog(savedInstanceState);
+            dialog.show();
+            ((TextView) dialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+        }
+    }
+
+    public class TermsAndConditionsDialogFragment extends DialogFragment {
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(GirisYapActivity.this);
+            builder.setCancelable(false);
+//            builder.setNegativeButton("Cancel",null);
+            builder.setMessage(Html.fromHtml("By using this app, you agree to our <a href=\"http://halitpractice.tech/legal/term.php\">Terms and Conditions</a> and <a href=\"http://halitpractice.tech/legal/policy.php\"> Privacy Policy</a>"))
+                    .setPositiveButton("I agree", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //
+                            SharedPreferences prefs = GirisYapActivity.this.getSharedPreferences(PLANT_PLACES_PREFS, Context.MODE_PRIVATE);
+                            SharedPreferences.Editor edit = prefs.edit();
+                            edit.putBoolean(TERMS_AND_CONDITIONS, true);
+                            edit.commit();
+                        }
+                    });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            return builder.create();
+        }
     }
 
     public void tanimla(){
